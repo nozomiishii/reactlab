9f3c1cdbef2af4b92ded84103372c49bf22b4ddb
Nozomi Ishii <non.ankt@gmail.com>
2020-05-29 16:39:21 +0900

    gsap ;

--- src/lab/r003/App.tsx
+++ src/lab/r003/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
-import { TimelineLite, Power3 } from "gsap";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
 
 type Props = {
   positionX: number;
@@ -11,39 +11,39 @@ const PhotoFlame = styled.div<Props>`
   position: absolute;
   top: ${(p) => `${p.positionY}px`};
   right: ${(p) => `${p.positionX}px`};
-  /* top: 20%;
-  left: 50%; */
 `;
 
 const Photo: React.FC = () => {
-  // const [imgData, setImgData] = useState("");
-  let positionX = Math.floor(Math.random() * window.innerWidth);
-  let positionY = Math.floor(Math.random() * window.innerHeight);
-  // useEffect(() => {
-  //   fetch("https://picsum.photos/200/300").then((response) =>
-  //     console.log(response.url)
-  //   );
-  // }, []);
-
   let randomId = Math.floor(Math.random() * 100);
   let photoId = `https://picsum.photos/id/${randomId}/200/300`;
+  let positionX = Math.floor(Math.random() * window.innerWidth);
+  let positionY = Math.floor(Math.random() * window.innerHeight);
   return (
     <PhotoFlame positionX={positionX} positionY={positionY}>
-      <img src={photoId} alt="randomimg" />
+      <img src={photoId} alt="random" />
     </PhotoFlame>
   );
 };
 
 const Container = styled.div`
+  visibility: hidden;
   width: 100vw;
   height: 100vh;
   position: relative;
   overflow: hidden;
 `;
 
-const App: React.FC = () => {
-  // let app = useRef(null);
+const Button = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100px;
+  height: 60px;
+`;
 
+const App: React.FC = () => {
+  let app = useRef(null);
   let photo1 = useRef<HTMLHeadingElement>(null);
   let photo2 = useRef<HTMLHeadingElement>(null);
   let photo3 = useRef<HTMLHeadingElement>(null);
@@ -52,11 +52,10 @@ const App: React.FC = () => {
   let photo6 = useRef<HTMLHeadingElement>(null);
   let photo7 = useRef<HTMLHeadingElement>(null);
   console.log(photo1);
-  // TweenMax.to(app, 0, { css: { visibility: "visible" } });
   let tl = new TimelineLite();
-  useEffect(() => {
-    // console.log(photo1, photo2, photo3, photo4, photo5, photo6, photo7);
 
+  const handleClick = () => {
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
     tl.staggerFrom(
       [photo1, photo2, photo3, photo4, photo5, photo6, photo7],
       1,
@@ -67,33 +66,35 @@ const App: React.FC = () => {
       },
       0.15
     );
-  }, [tl]);
+  };
 
   return (
-    // <Container ref={(el: any) => (app = el)}>
-    <Container>
-      <div ref={(el: any) => (photo1 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo2 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo3 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo4 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo5 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo6 = el)}>
-        <Photo />
-      </div>
-      <div ref={(el: any) => (photo7 = el)}>
-        <Photo />
-      </div>
-    </Container>
+    <>
+      <Container ref={(el: any) => (app = el)}>
+        <div ref={(el: any) => (photo1 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo2 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo3 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo4 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo5 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo6 = el)}>
+          <Photo />
+        </div>
+        <div ref={(el: any) => (photo7 = el)}>
+          <Photo />
+        </div>
+      </Container>
+      <Button onClick={handleClick}>Click!</Button>
+    </>
   );
 };
 
